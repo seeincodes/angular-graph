3a4fd2eed071da2600a5d0297a2045c295f60324
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-10-14 14:20:07 -0500

    Making it work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,38 @@ import { GraphComponent } from './graph/graph.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular'
+import { HttpLink } from 'apollo-angular/http'
+import { InMemoryCache } from '@apollo/client/core'
+
+
 @NgModule({
   declarations: [
     AppComponent,
     GraphComponent
   ],
   imports: [
+    ApolloModule,
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        console.log('loading graphql')
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://api.thegraph.com/subgraphs/name/messari/balancer-v2-polygon'
+          })
+        }
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
